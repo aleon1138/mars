487b089b8918a4a8a516c9f4ffe015092da7991a
Arnaldo Leon <amleon@alum.mit.edu>
2026-03-19 17:19:08 -0500

    Cleanup of pyproject and setup scripts

--- setup.py
+++ setup.py
@@ -14,21 +14,17 @@ define_macros = [
     ("EIGEN_DONT_PARALLELIZE", "1"),
 ]
 
-module = Extension(
-    "marslib",
-    extra_compile_args=extra_compile_args,
-    extra_link_args=["-fopenmp"],
-    include_dirs=[pybind11.get_include(), "/usr/include/eigen3"],
-    sources=["marslib.cc"],
-    define_macros=define_macros,
-    language="c++",
-)
 
 setup(
-    name="mars",
-    version="0.3",
-    author="Arnaldo Leon",
-    description="Multivariate Adaptive Regression Splines",
-    py_modules=["mars"],
-    ext_modules=[module],
+    ext_modules=[
+        Extension(
+            "marslib",
+            sources=["marslib.cc"],
+            include_dirs=[pybind11.get_include(), "/usr/include/eigen3"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=["-fopenmp"],
+            define_macros=define_macros,
+            language="c++",
+        )
+    ],
 )
